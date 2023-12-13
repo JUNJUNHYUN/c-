74c3469f4c59cdd78d6dfc0ae1db434d765cca65
JUNJUNHYUN <153727999+JUNJUNHYUN@users.noreply.github.com>
2023-12-13 21:50:32 +0900

    Add files via upload

---
+++ 1213_01.cpp
@@ -0,0 +1,10 @@
+//standard input & output
+#include<stdio.h>
+
+//int-main entry-point
+int main() {
+	printf("Hello world!\n");
+	printf("어떤 분야든 잘 수행하는 개발자를 하고 싶습니다.");
+	return 0;
+}
+
---
+++ 1213_02.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	// 서식 지정자
+	// %d 정수된 숫자
+	printf("%d%d%d%d%d\n",1,1,1,1,1);
+
+	printf("%d + %d = %d\n", 5, 10, 15);
+	printf("%d * %d = %d", 2, 3, 6);
+
+	return 0;
+}
